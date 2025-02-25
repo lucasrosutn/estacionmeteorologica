608b843f90ab605a340f5c0fb495639f98df519d
Lucas Rosemberg <lucasrosemberg@MacBook-Air-de-Lucas.local>
2025-02-25 16:30:10 -0300

    variables related to delay times and delays itself modified to shorten the welcome screen time

--- src/led.cpp
+++ src/led.cpp
@@ -3,24 +3,24 @@
 
 
 void welcome(void){
-    for(int i=0;i<10;i++){
+    for(int i=0;i<5;i++){
         digitalWrite(LED_PIN,HIGH);
-        delay(80);
+        delay(40);
         digitalWrite(LED_PIN,LOW);
-        delay(80);
+        delay(40);
     }
-    delay(2000);
+    delay(500);
   }
 
 
 
 
   void blink_led(uint8_t led_pin, float temperature, float humidity) {
-    if (temperature > 20.0 && humidity > 20.0) {  
+    if (temperature > 23.0 && humidity > 40.0) {  
         digitalWrite(led_pin, HIGH);
         delay(2000);
         digitalWrite(led_pin, LOW);
-        delay(1000);
+        delay(4000);
     } else {
         digitalWrite(led_pin, LOW);  // Apaga el LED si no cumple la condición
     }
